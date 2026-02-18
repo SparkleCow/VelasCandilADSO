c8b3ad66b863a1c9e2e550018d2ca2b4dadfce01
SparkleCow <cjelcazador200@outlook.com>
2026-02-18 16:10:04 -0500

    creating project

---
+++ candil-api/src/main/java/com/velas/candil/VelasCandilApplication.java
@@ -0,0 +1,13 @@
+package com.velas.candil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelasCandilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelasCandilApplication.class, args);
+	}
+
+}
---
+++ candil-api/src/test/java/com/velas/candil/VelasCandilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velas.candil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelasCandilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
